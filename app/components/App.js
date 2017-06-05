ee7ee684d8be2d92643c80524a03b1d9dedd2438
Leo <leo.grivel@epitech.eu>
2017-06-05 23:06:11 +0200

    Pads added, they are now displayed correctly

--- app/components/App.js
+++ app/components/App.js
@@ -5,21 +5,47 @@ import {
   StyleSheet
 } from 'react-native';
 
+const PAD_NUMBER = 4;
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    this.props.getProps();
+  renderPads() {
+    const renderOnePad = j => {
+      const pads = [];
+      for (let i = 0; i < PAD_NUMBER; i += 1) {
+        const padNumber = j * PAD_NUMBER + i;
+        pads.push(
+          <View
+            key={`Pad${i}${j}`}
+            style={styles.pad}
+          />
+        );
+      }
+
+      return pads;
+    };
+
+    const pads = [];
+    for (let i = 0;i < PAD_NUMBER; i += 1) {
+      pads.push(
+        <View
+          style={{flex: 1, alignSelf: 'stretch'}}
+          key={`Line${i}`}
+        >
+          {renderOnePad(i)}
+        </View>
+      );
+    }
+
+    return pads;
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {(this.props.defaultProps !== undefined)
-          ? (<Text style={styles.message}>{this.props.defaultProps}</Text>)
-          : (<Text style={styles.message}>{'Props are loading...'}</Text>)}
+        {this.renderPads()}
       </View>
     );
   }
@@ -29,11 +55,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  message: {
-    fontSize: 30,
+    alignItems: 'center',
+    flexDirection: 'row',
     padding: 10
+  },
+  pad: {
+    backgroundColor: 'gray',
+    flex: 1,
+    borderRadius: 5,
+    margin: 5
   }
 });
 
